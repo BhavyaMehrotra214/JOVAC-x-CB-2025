80b7f272d12fe09fe50441c7fc43fff0874a8ad0
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-06-18 23:13:21 +0530

    function

--- Javascript_150625/index.js
+++ Javascript_150625/index.js
@@ -1,16 +1,67 @@
-var x =1;
-a();
-b();
-console.log(x);
-function a(){
-    var x = 10;
-    console.log(x);
-}
-function b(){
-    var x = 100;
-    console.log(x);
-}
+//var x =1;
+//a();
+//b();
+//console.log(x);
+//function a(){
+//  var x = 10;
+//  console.log(x);
+//}
+//function b(){
+//  var x = 100;
+//  console.log(x);
+//}
+
 //document.getElementById("functionCall").addEventListener('click',function abc(){
-  //  console.log("Event happened!")
+//  console.log("Event happened!")
 //})
 
+// var xyz = function abc(){
+//  console.log(10);
+// }
+// xyz();
+// console.log(xyz);
+
+//var xyz = function abc(){
+    
+//}
+//console.log(xyz);
+//console.log(abc);
+
+
+//function attachEventListerner(){
+//   let count =0;
+//    document.getElementById('click').addEventListener('click',function xyz(){
+    //    console.log("button clicked",++count);
+   // });
+//}
+//attachEventListerner();
+
+
+
+//function attachEventListerner() {
+ //   let count = 0;
+
+
+ //   for (let i = 0; i < 100; i++) {
+  //      document.getElementById('click').addEventListener('click', function () {
+  //          console.log("Listener", i + 1, "button clicked", ++count);
+  //      });
+  //  }
+//}
+
+//attachEventListerner();
+
+//function createObject(){
+ //     let  person = {
+ //           name: 'jenny',
+ //            age:  25
+  //       }
+  //  console.log(person.name);
+//}
+//createObject();
+
+var myFun = function logMessage(){
+  console.log("hello world");
+}
+myFun();
+console.log(logMessage);
