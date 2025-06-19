a2a67a5e542e4243d5d9d8d6974a47c7f644f608
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-06-19 20:40:48 +0530

    operations

--- javascript_practice/hello.js
+++ javascript_practice/hello.js
@@ -2,6 +2,10 @@ let gameName = "spiderman";
 gameName = "batsman";
 console.log(gameName);
 
+//error//
 const username = "bhavay14";
 username = "bhavya";
 console.log(username);
+
+//operations//
+let score =102;
