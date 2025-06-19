4a027976386dde0c7db82946872a6e059b740261
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-06-19 21:56:22 +0530

    file added

--- javascript_practice/hello.js
+++ javascript_practice/hello.js
@@ -1,11 +1,24 @@
-let gameName = "spiderman";
-gameName = "batsman";
-console.log(gameName);
+// let gameName = "spiderman";
+// gameName = "batsman";
+// console.log(gameName);
 
 //error//
-const username = "bhavay14";
-username = "bhavya";
-console.log(username);
+// const username = "bhavay14";
+// username = "bhavya";
+// console.log(username);
 
 //operations//
-let score =102;
+// let score =102;
+
+//var x =1;
+//a();
+//b();
+//console.log(x);
+//function a(){
+//  var x = 10;
+//  console.log(x);
+//}
+//function b(){
+//  var x = 100;
+//  console.log(x);
+//}
