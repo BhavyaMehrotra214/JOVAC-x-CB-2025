3715a1c62e1c912a1863000ae65007bc6bd632b2
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-06-24 20:52:55 +0530

    project1

--- project_1/script.js
+++ project_1/script.js
@@ -11,6 +11,15 @@ userdata.then(function(response){
       document.getElementById("bio").textContent = data.bio;
        document.getElementById("Followers").textContent = data.followers;
        document.getElementById("repos").textContent = data.public_repos;     
+       return fetch(data.repos_url);
+    })
+.then(function(response){
+    return response.json();
+})
+.then(function(reposData){
+    for(let i =0;i<3 && i < reposData.length; i++){
+        document.getElementById("repo-list").innerHTML += `<li>${reposData[i].name}</li>`
+    }
 })
 .catch(function(handleerror){
     console.log("error:",handleerror)
